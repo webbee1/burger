90a540ff5c0dfb48c6bb93e3dcb6f5f4903c9c43
webbee1 <hell.web@yandex.ru>
2018-11-07 14:38:43 +0200

    add api map

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+    ymaps.ready(init);
+    function init(){  
+        var myMap = new ymaps.Map("map", {
+            center: [59.89,30.42],
+            zoom: 10,
+            controls: []       
+        });
+        
+        var coords =[
+            [59.98,30.29], 
+            [59.87, 30.45], 
+            [59.92, 30.51], 
+            [59.90, 30.31]
+        ];
+    
+        var myCollection = new ymaps.GeoObjectCollection({}, {
+          iconLayout: 'default#image',
+          iconImageHref: '../images/icons/map-marker.svg',
+          iconImageSize: [46, 57]       
+      });
+
+      coords.forEach(function (item) {
+        myCollection.add(new ymaps.Placemark(item));
+      });
+
+      myMap.geoObjects.add(myCollection)
+     myMap.behaviors.disable(['scrollZoom']);
+
+    }
