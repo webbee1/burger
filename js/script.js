3f1f7aa5650d1d655e9a350b075842540b059fd0
webbee1 <hell.web@yandex.ru>
2018-11-22 22:14:35 +0200

    slider bad version

--- js/script.js
+++ js/script.js
@@ -43,4 +43,59 @@ var btn = document.querySelector('.hamburger__btn');
 
     //slider
 
-    
+    const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const slider__all = document.querySelector("#show");
+const computed = getComputedStyle(slider__all);
+
+//при обработчике клика на кнопку делаем preventDefault, 
+//чтоб не подбрасывало страницу вверх
+
+right.addEventListener("click", function(e) {
+  e.preventDefault();
+
+   //получаем текущее значение right для выполнения след.шага
+  //на какой позиции находимся?
+
+  let currentRight = parseInt(computed.right);
+
+  if (!currentRight) {
+    currentRight = 0;
+  }
+  
+
+ //берём текущее значение для свойства right и прибавляем к нему ширину 
+  //и переводим в пиксели
+  //если текущая координата мееньше 3880 - значит ещё не конец слайдов
+
+  if (currentRight < 3880) {
+    slider__all.style.right = currentRight + 970 + "px";
+  } else {
+    currentRight = 0;
+    slider__all.style = 0;
+  }
+});
+
+
+//добавляем обработчики событий на кнопки 
+
+left.addEventListener("click", function(e) {
+  e.preventDefault();
+  let currentRight = parseInt(computed.right);
+
+  if (!currentRight) {
+    currentRight = 0;
+  }
+
+  //зацикливаем слайдер
+
+  if (currentRight > 0) {
+    slider__all.style.right = currentRight - 970 + "px";
+  } else {
+    currentRight = 3880;
+    slider__all.style = currentRight + 'px';
+  }
+});
+
+
+
